72d4bbeb7f1a76cd210bc99227a9b13f2d366a28
Gimenez Santiago <santiago.manuel.gimenez97@gmail.com>
2023-10-16 21:41:55 -0300

    test

--- src/App.jsx
+++ src/App.jsx
@@ -22,7 +22,7 @@ function App() {
     portafolioRef.current.scrollIntoView({ behavior: 'smooth' });
   };
 
-  const [navOn, setNavOn] = useState(false)
+  const [navOn, setNavOn] = useState(true)
 
 
   return (
