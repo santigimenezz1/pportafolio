2dc6b551ca5bee04d27ebfd4a7476381a61b1dad
Gimenez Santiago <santiago.manuel.gimenez97@gmail.com>
2023-10-22 17:36:46 -0300

    agregando proyecto

--- src/Components/Portfolio/Portfolio.jsx
+++ src/Components/Portfolio/Portfolio.jsx
@@ -11,7 +11,7 @@ const Portfolio = () => {
     <TarjetaPortfolio urlImagen={"https://res.cloudinary.com/dcf9eqqgt/image/upload/v1697119380/portafolio/habilidades/cascanueces_v3_qdofay.png"} demo={"https://cascanueces-ue9o.vercel.app/"}/>
     <TarjetaPortfolio urlImagen={"https://res.cloudinary.com/dcf9eqqgt/image/upload/v1697122155/portafolio/habilidades/tienda_digital_v1_nuuv38.png"} demo={"https://tiendadigital.vercel.app/"} />
     <TarjetaPortfolio urlImagen={"https://res.cloudinary.com/dcf9eqqgt/image/upload/v1697134008/portafolio/habilidades/spotyclon_qognsi.png"}  />
-    <TarjetaPortfolio />
+    <TarjetaPortfolio urlImagen={"https://res.cloudinary.com/dcf9eqqgt/image/upload/v1698006749/portafolio/clima_2_yggudl.png"}  demo={"https://app-del-clima-two.vercel.app/"}/>
 
  
     </div>
