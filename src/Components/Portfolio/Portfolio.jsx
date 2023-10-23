fe5c40147fc4e69e655c9dc6e1c1bddb2f53e85c
Gimenez Santiago <santiago.manuel.gimenez97@gmail.com>
2023-10-23 11:01:52 -0300

    test

--- src/Components/Portfolio/Portfolio.jsx
+++ src/Components/Portfolio/Portfolio.jsx
@@ -20,7 +20,7 @@ const Portfolio = () => {
     <TarjetaMobile urlImagen={"https://res.cloudinary.com/dcf9eqqgt/image/upload/v1697119380/portafolio/habilidades/cascanueces_v3_qdofay.png"} demo={"https://cascanueces-ue9o.vercel.app/"} description={"e-comerce para una tienda de la cuidad de rosario, la cual contiene registro de usuarios, y un app independiente para agregar, editar o eliminar los productos"}/>
     <TarjetaMobile urlImagen={"https://res.cloudinary.com/dcf9eqqgt/image/upload/v1697122155/portafolio/habilidades/tienda_digital_v1_nuuv38.png"} demo={"https://tiendadigital.vercel.app/"} description={"e-comerce de una tienda de productos para gaming, en el que cualquier comprador puede agregar sus productos a un carrito y enviarle el pedido al local para gestionar su envio a domicilio"}/>
     <TarjetaMobile urlImagen={"https://res.cloudinary.com/dcf9eqqgt/image/upload/v1697134008/portafolio/habilidades/spotyclon_qognsi.png"}  description={"Clon de spotify, en el cual cada usuario puede agregar sus artitas preferidos, agregar albunes y canciones y tener una seccion de canciones favoritas"}/>  
-    <TarjetaMobile urlImagen={"https://res.cloudinary.com/dcf9eqqgt/image/upload/v1698007104/portafolio/clima3_rwwmf0.png"}  demo={"https://app-del-clima-two.vercel.app/"}/>
+    <TarjetaMobile urlImagen={"https://res.cloudinary.com/dcf9eqqgt/image/upload/v1698069483/portafolio/WhatsApp_Image_2023-10-23_at_10.56.58_d0ycus.jpg"}  demo={"https://app-del-clima-two.vercel.app/"}/>
 
     </div>
     </div>
