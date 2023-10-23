ae42866db57d1661dec83e809dc6364e1710c488
Gimenez Santiago <santiago.manuel.gimenez97@gmail.com>
2023-10-23 11:27:22 -0300

    test

--- src/Components/Portfolio/Portfolio.jsx
+++ src/Components/Portfolio/Portfolio.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import TarjetaPortfolio from './TarjetaPortfolio/TarjetaPortfolio'
 import '../Portfolio/portfolio.css'
 import TarjetaMobile from './TarjetaMobile/TarjetaMobile'
+import TarjetaMobilev2 from './TarjetaMobilev2/TarjetaMobilev2'
 
 const Portfolio = () => {
   return (
@@ -21,8 +22,9 @@ const Portfolio = () => {
     <TarjetaMobile urlImagen={"https://res.cloudinary.com/dcf9eqqgt/image/upload/v1697122155/portafolio/habilidades/tienda_digital_v1_nuuv38.png"} demo={"https://tiendadigital.vercel.app/"} description={"e-comerce de una tienda de productos para gaming, en el que cualquier comprador puede agregar sus productos a un carrito y enviarle el pedido al local para gestionar su envio a domicilio"}/>
     <TarjetaMobile urlImagen={"https://res.cloudinary.com/dcf9eqqgt/image/upload/v1697134008/portafolio/habilidades/spotyclon_qognsi.png"}  description={"Clon de spotify, en el cual cada usuario puede agregar sus artitas preferidos, agregar albunes y canciones y tener una seccion de canciones favoritas"}/>  
     <TarjetaMobile urlImagen={"https://res.cloudinary.com/dcf9eqqgt/image/upload/v1698007104/portafolio/clima3_rwwmf0.png"}  demo={"https://app-del-clima-two.vercel.app/"}/>
-
+    
     </div>
+    <TarjetaMobilev2/>
     </div>
   )
 }
---
+++ src/Components/Portfolio/TarjetaMobilev2/TarjetaMobilev2.jsx
@@ -0,0 +1,14 @@
+import '../TarjetaMobilev2/tarjetaMobilev2.css'
+const TarjetaMobilev2 = () => {
+  return (
+    <div className='tarjetaMobilev2'>
+    <div className='tarjetaMobilev2__imagen'>
+    <img src='https://res.cloudinary.com/dcf9eqqgt/image/upload/v1697119380/portafolio/habilidades/cascanueces_v3_qdofay.png'></img>
+    </div>
+    <div className='tarjetaMobilev2__info'>
+    </div>
+    </div>
+  )
+}
+
+export default TarjetaMobilev2
