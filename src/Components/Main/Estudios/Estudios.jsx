054c4627ca009e369f28d2843d8f164eb63a5050
santiago gimenez <santiago.manuel.gimenez97@gmail.com>
2023-12-28 19:59:13 -0300

    correccion de logos

--- src/Components/Main/Estudios/Estudios.jsx
+++ src/Components/Main/Estudios/Estudios.jsx
@@ -8,9 +8,9 @@ const Estudios = () => {
       <Cursos text={"Curso de desarrollo web"} urlCurso={"https://res.cloudinary.com/dcf9eqqgt/image/upload/v1697116999/portafolio/habilidades/nx2vppdwkspzyle9z070.png"} />
       <Cursos text={"Curso de JavaScript"} urlCurso={"https://res.cloudinary.com/dcf9eqqgt/image/upload/v1697116999/portafolio/habilidades/nx2vppdwkspzyle9z070.png"} />
       <Cursos text={"Curso de React"} urlCurso={"https://res.cloudinary.com/dcf9eqqgt/image/upload/v1697116999/portafolio/habilidades/nx2vppdwkspzyle9z070.png"} />
-      <Cursos text={"Curso Next.js"} urlCurso={"https://res.cloudinary.com/dcf9eqqgt/image/upload/v1697116999/portafolio/habilidades/nx2vppdwkspzyle9z070.png"} />
-      <Cursos text={"Curso Intensivo de Figma"} urlCurso={"https://res.cloudinary.com/dcf9eqqgt/image/upload/v1697116999/portafolio/habilidades/nx2vppdwkspzyle9z070.png"} />
-      <Cursos text={"Javascript avanzado-Soy henry"} urlCurso={"https://res.cloudinary.com/dcf9eqqgt/image/upload/v1697116999/portafolio/habilidades/nx2vppdwkspzyle9z070.png"} />
+      <Cursos text={"Curso Next.js"} urlCurso={"https://res.cloudinary.com/dcf9eqqgt/image/upload/v1703804304/portafolio/YouTube_social_white_square__2017_.svg_kjcxes.png"} />
+      <Cursos text={"Curso Intensivo de Figma"} urlCurso={"https://res.cloudinary.com/dcf9eqqgt/image/upload/v1703804304/portafolio/YouTube_social_white_square__2017_.svg_kjcxes.png"} />
+      <Cursos text={"Javascript avanzado-Soy henry"} urlCurso={"https://res.cloudinary.com/dcf9eqqgt/image/upload/v1703804227/portafolio/logoOG_r1lwkn.png"} />
 
 
       
