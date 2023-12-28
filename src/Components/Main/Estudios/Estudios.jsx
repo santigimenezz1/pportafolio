7559a4bb3e249033604eef5116eebdcced13c6fd
santiago gimenez <santiago.manuel.gimenez97@gmail.com>
2023-12-28 20:08:11 -0300

    correccion de estudios

--- src/Components/Main/Estudios/Estudios.jsx
+++ src/Components/Main/Estudios/Estudios.jsx
@@ -11,6 +11,8 @@ const Estudios = () => {
       <Cursos text={"Curso Next.js"} urlCurso={"https://res.cloudinary.com/dcf9eqqgt/image/upload/v1703804304/portafolio/YouTube_social_white_square__2017_.svg_kjcxes.png"} />
       <Cursos text={"Curso Intensivo de Figma"} urlCurso={"https://res.cloudinary.com/dcf9eqqgt/image/upload/v1703804304/portafolio/YouTube_social_white_square__2017_.svg_kjcxes.png"} />
       <Cursos text={"Javascript avanzado"} urlCurso={"https://res.cloudinary.com/dcf9eqqgt/image/upload/v1703804227/portafolio/logoOG_r1lwkn.png"} />
+      <Cursos text={"Curso de Sass"} urlCurso={"https://res.cloudinary.com/dcf9eqqgt/image/upload/v1703804304/portafolio/YouTube_social_white_square__2017_.svg_kjcxes.png"} />
+
 
 
       
