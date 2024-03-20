641e8eb245dfc99cf07d95d9a2cd1d19a75bda58
santiago <santiago.manuel.gimenez97@gmail.com>
2024-03-20 14:50:14 +0100

    SECCION ESTUDIOS ACTUALIZADA

--- src/Components/Main/Estudios/Estudios.jsx
+++ src/Components/Main/Estudios/Estudios.jsx
@@ -5,13 +5,9 @@ import '../Estudios/estudios.css'
 const Estudios = () => {
   return (
     <div className='estudios'>
-      <Cursos text={"Curso de desarrollo web"} urlCurso={"https://res.cloudinary.com/dcf9eqqgt/image/upload/v1697116999/portafolio/habilidades/nx2vppdwkspzyle9z070.png"} />
-      <Cursos text={"Curso de JavaScript"} urlCurso={"https://res.cloudinary.com/dcf9eqqgt/image/upload/v1697116999/portafolio/habilidades/nx2vppdwkspzyle9z070.png"} />
-      <Cursos text={"Curso de React"} urlCurso={"https://res.cloudinary.com/dcf9eqqgt/image/upload/v1697116999/portafolio/habilidades/nx2vppdwkspzyle9z070.png"} />
-      <Cursos text={"Curso Next.js"} urlCurso={"https://res.cloudinary.com/dcf9eqqgt/image/upload/v1703804304/portafolio/YouTube_social_white_square__2017_.svg_kjcxes.png"} />
-      <Cursos text={"Curso Intensivo de Figma"} urlCurso={"https://res.cloudinary.com/dcf9eqqgt/image/upload/v1703804304/portafolio/YouTube_social_white_square__2017_.svg_kjcxes.png"} />
-      <Cursos text={"Javascript avanzado"} urlCurso={"https://res.cloudinary.com/dcf9eqqgt/image/upload/v1703804227/portafolio/logoOG_r1lwkn.png"} />
-      <Cursos text={"Curso de Sass"} urlCurso={"https://res.cloudinary.com/dcf9eqqgt/image/upload/v1703804304/portafolio/YouTube_social_white_square__2017_.svg_kjcxes.png"} />
+      <Cursos text={"Carrera de desarrollo Frontend ReactJS - CODERHOUSE" } urlCurso={"https://res.cloudinary.com/dcf9eqqgt/image/upload/v1710941959/portafolio/logo_coder_ljbfrx.jpg"} />
+      <Cursos text={"Curso Next.js - CODERHOUSE"} urlCurso={"https://res.cloudinary.com/dcf9eqqgt/image/upload/v1710941959/portafolio/logo_coder_ljbfrx.jpg"} />
+      <Cursos text={"Javascript avanzado - Bootcamp SOY HENRY"} urlCurso={"https://res.cloudinary.com/dcf9eqqgt/image/upload/v1703804227/portafolio/logoOG_r1lwkn.png"} />
 
 
 
